3a145ee7ec673f2e19decafa0fac6bbb909383c4
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2022-12-16 23:44:19 +0100

    feat(info) add activity

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,8 @@ import api.welcomeChannel
 import commands.*
 import event.MemberEvent
 import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.OnlineStatus
+import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.requests.GatewayIntent
 
 fun main(args: Array<String>) {
@@ -14,6 +16,7 @@ fun main(args: Array<String>) {
         .addEventListeners(MemberEvent(), RegisterEvent())
         .build()
     api.awaitReady()
+    api.presence.setPresence(OnlineStatus.ONLINE, Activity.watching("Le Monde de la Nuit"), false)
     val guild = api.guilds.first()!!
     roleOfMember = guild.getRoleById(args[1]) ?: throw Exception("Role not found")
     welcomeChannel = guild.getTextChannelById(args[2]) ?: throw Exception("Channel not found")
